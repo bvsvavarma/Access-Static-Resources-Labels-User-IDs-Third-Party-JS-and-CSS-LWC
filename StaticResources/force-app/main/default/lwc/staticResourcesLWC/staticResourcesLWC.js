ff982945d40449d373c6c10fd66f49f786b01928
satyabuddharajuvenkata <buddharaju.varma@icloud.com>
2025-02-25 13:54:35 -0600

    final version

--- StaticResources/force-app/main/default/lwc/staticResourcesLWC/staticResourcesLWC.js
+++ StaticResources/force-app/main/default/lwc/staticResourcesLWC/staticResourcesLWC.js
@@ -7,6 +7,10 @@ import USER_ID from '@salesforce/user/Id';
 import { getFieldValue, getRecord } from 'lightning/uiRecordApi';
 import NAME_FIELD from '@salesforce/schema/User.Name';
 import DISPLAY_TEXT from '@salesforce/customPermission/Display_Text';
+import { loadStyle, loadScript } from 'lightning/platformResourceLoader';
+import THIRD_PARTY_CSS from '@salesforce/resourceUrl/ThirdPartyCSS';
+import THIRD_PARTY_JS from '@salesforce/resourceUrl/ThirdPartyJS';
+
 
 export default class StaticResourcesLWC extends LightningElement {
     //we can't directly access imports
@@ -17,6 +21,8 @@ export default class StaticResourcesLWC extends LightningElement {
         platform : SALESFORCE_PLATFORM
     };
     userName = "";
+    isFirstLoad = true;
+    displayDate = "";
 
 
     @wire(getRecord,{
@@ -33,5 +39,24 @@ export default class StaticResourcesLWC extends LightningElement {
 
     get checkPermission(){
         return DISPLAY_TEXT;
+    }
+
+    renderedCallback(){
+        if(this.isFirstLoad){
+            this.isFirstLoad = false;
+            Promise.all([loadScript(this, THIRD_PARTY_JS),
+                loadStyle(this, THIRD_PARTY_CSS)])
+            .then(() => {
+                console.log('File Load Successfully');   
+                this.fetchDate();
+            })
+            .catch((error) => {
+                console.log('Files Load Failure', error);
+            })
+        }
+    }
+
+    fetchDate(){
+        this.displayDate = moment().format("LLLL");
     }
 }
