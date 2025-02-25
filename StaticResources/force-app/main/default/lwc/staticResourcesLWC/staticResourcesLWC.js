b28aff28f7ff2756390cd4602e26d467ca49b2d2
satyabuddharajuvenkata <buddharaju.varma@icloud.com>
2025-02-25 11:51:49 -0600

    staticResource changes

--- StaticResources/force-app/main/default/lwc/staticResourcesLWC/staticResourcesLWC.js
+++ StaticResources/force-app/main/default/lwc/staticResourcesLWC/staticResourcesLWC.js
@@ -1,3 +1,37 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire } from 'lwc';
+import LOGO from '@salesforce/resourceUrl/MySFLogo';
+import myContentAsset from "@salesforce/contentAssetUrl/imagespng";
+import SALESFORCE_PLATFORM from '@salesforce/label/c.salesforcePlatform';
+import GREETING from '@salesforce/label/c.greeting';
+import USER_ID from '@salesforce/user/Id';
+import { getFieldValue, getRecord } from 'lightning/uiRecordApi';
+import NAME_FIELD from '@salesforce/schema/User.Name';
+import DISPLAY_TEXT from '@salesforce/customPermission/Display_Text';
 
-export default class StaticResourcesLWC extends LightningElement {}
+export default class StaticResourcesLWC extends LightningElement {
+    //we can't directly access imports
+    mySalesForceLogo = LOGO;
+    myContentAssetLogo = myContentAsset;
+    labels = {
+        greeting : GREETING, 
+        platform : SALESFORCE_PLATFORM
+    };
+    userName = "";
+
+
+    @wire(getRecord,{
+        recordId : USER_ID,
+        fields: [NAME_FIELD]
+    }) wired_userDetails({data, error}){
+        if(data){
+            console.log('User Info Details', data);
+            this.userName = getFieldValue(data, NAME_FIELD);
+        }else if(error){
+            console.log('User Info Details error', error);
+        }
+    }
+
+    get checkPermission(){
+        return DISPLAY_TEXT;
+    }
+}
